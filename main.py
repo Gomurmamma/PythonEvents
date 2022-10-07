0f3a28d79a89449a1739f9fd515a07845b5f72a2
merritcl <merritcl@oregonstate.edu>
2022-10-07 14:07:38 -0400

    other enviornment variables

--- main.py
+++ main.py
@@ -3,16 +3,24 @@
 # Clinton Merritt
 
 
-import smtplib, ssl, email, requests
+import smtplib, ssl, email, requests, os
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 # Create MIMEMultipart object
 msg = MIMEMultipart("alternative")
 msg["Subject"] = "multipart test"
-msg["From"] = sender_email
-msg["To"] = receiver_email
+msg["From"] = os.getenv('sender_email')
+msg["To"] = os.getenv('receiver_email')
 
+# other enviornment variables
+client_id = os.getenv('client_id')
+client_secret = os.getenv('client_secret')
+password = os.getenv('password')
 
+print(password)
