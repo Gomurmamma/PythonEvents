a660159647365a3917d5678103654d5736e43d5a
merritcl <merritcl@oregonstate.edu>
2022-10-07 12:48:15 -0400

    MIME formatting

--- main.py
+++ main.py
@@ -4,4 +4,8 @@
 
 
 import smtplib, ssl, email, requests
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
