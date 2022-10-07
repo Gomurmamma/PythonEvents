0cda89923755d67464425e1a05cf23203c673a9b
Donald Antonio Young <youngdo@oregonstate.edu>
2022-10-07 14:48:53 -0400

    event info loop

--- main.py
+++ main.py
@@ -30,3 +30,36 @@ password = os.getenv('password')
 # Per page: 10 (default)
 # page: 1 (default)
 response = requests.get(f"https://api.seatgeek.com/2/events?lat=40.650002&lon=-73.949997&client_id={client_id}&type=concert&&client_secret={client_secret}")
+
+events = response.json()['events']
+
+for event in events:
+
+    for info in event:
+
+        title = event['title']
+        ticket_url = event['url']
+
+        # date needs to be formatted
+        date = event['datetime_local']
+
+        venue = event['venue']
+
+        venue_name = venue['name']
+        city = venue['city']
+        state = venue['state']
+
+        performers = event['performers']
+
+        for performer in performers:
+
+            image_url = performer['image']
+
+            print(title)
+            print(ticket_url)
+            print(date)
+            print(venue_name)
+            print(city)
+            print(state)
+            print(image_url)
+
